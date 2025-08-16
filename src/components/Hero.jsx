75a1a20669950acc738c702217b2b95f1818c34f
Socheath <freshlume@gmail.com>
2025-08-16 16:12:30 +0700

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
 
   return (
     <section id='home' className='relative min-h-screen flex items-center justify-center px-3 text-white overflow-hidden'>
-      {/* Animated gradient background */}
+      {/* Animated gradient background*/}
       <div className='relative z-10 text-center max-w-4xl px-4'>
         <h1 className='text-5xl md:text-8xl font-black text-white mb-2 tracking-tight'>
           <span className='inline-block transition-all duration-500 ease-in-out py-3'>{greeting}</span>,{' '}
